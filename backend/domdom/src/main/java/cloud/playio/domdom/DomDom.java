644e3d38e9b792270f9533a8b547ddbc5c702027
zero88 <sontt246@gmail.com>
2023-01-27 21:21:06 +0700

    [:init] Fix build

---
+++ backend/domdom/src/main/java/cloud/playio/domdom/DomDom.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023. https://playio.cloud/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * All rights reserved.
+ */
+
+package cloud.playio.domdom;
+
+public class DomDom {}
